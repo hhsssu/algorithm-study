bd958ae73330d161406b98237d59b82b869471d1
Seonju Hong <87113802+hhsssu@users.noreply.github.com>
2025-03-21 15:23:06 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 1+2+3+4+5+6+7+8+9;
+        int num = 0;
+        for(int i = 0; i < numbers.length; i++) {            
+            for(int j = 0; j <= 9; j++) {
+                if(numbers[i] == j) {
+                    answer -= numbers[i];
+                    break;
+                }                
+            }
+        }
+        
+        return answer;
+    }
+}
